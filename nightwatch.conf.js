970a73a0f8e45af7425b2c8a9cae00baaa309301
xianpeng.shen <xshen@rocketsoftware.com>
2018-05-08 17:29:37 +0800

    change travis.yml node version

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -32,7 +32,6 @@ const config = {
       "desiredCapabilities": {
         "browserName": "chrome",
         "acceptSslCerts": true,
-        "marionette": false,
         "chromeOptions" : {
           "args" : ["chrome"] //chrome, headless
         }
