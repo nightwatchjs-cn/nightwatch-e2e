a21b62eebf776c9dd78a39431662be885d7fcaaa
xianpeng.shen <xshen@rocketsoftware.com>
2018-05-08 15:01:52 +0800

    fix a part of TC45

--- tests/TC/TC45.js
+++ tests/TC/TC45.js
@@ -3,3 +3,19 @@
  * @link https://app.testquality.com/project/2689/testcase/59737
  */
 
+module.exports = {
+
+    before: function(client) {
+        client.login();
+    },
+
+    '@tags': ['home'],
+    'login home page and verify': function(client) {
+        const navbar = client.page.navbar();
+        navbar.verifyVesion(process.env.VERSION);
+    },
+
+    after: function(client) {
+        client.end();
+    }
+};
