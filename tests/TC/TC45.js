f52b3836cd157a10d8b16e2aadbd96c0909da61c
xianpeng.shen <xshen@rocketsoftware.com>
2018-05-25 10:47:22 +0800

    fix Travis build error

--- tests/TC/TC45.js
+++ tests/TC/TC45.js
@@ -1,5 +1,5 @@
 /**
- * @author Xianpeng
+ * @author Xianpeng Shen
  * @link https://app.testquality.com/project/2689/testcase/59737
  */
 
@@ -11,8 +11,18 @@ module.exports = {
 
     '@tags': ['home'],
     'login home page and verify': function(client) {
-        const navbar = client.page.navbar();
+        var navbar = client.page.navbar();
         navbar.verifyVesion(process.env.VERSION);
+
+        var homePage = client.page.home();
+        homePage.expect.section('@home').to.be.visible;
+
+        // var homeSetion = homePage.section.home;
+        // homeSetion.expect.element('@jumbotron').to.be.visible;
+        // homeSetion.expect.element('@download').to.be.visible;
+        // homeSetion.expect.element('@mainFeatures').to.be.visible;
+        // homeSetion.expect.element('@example').to.be.visible;
+        // homeSetion.expect.element('@twitter').to.be.visible;
     },
 
     after: function(client) {
